12bdfa071478442e89212525c81f75ce42947ba5
Rafi Abbel <masterraf21@gmail.com>
2021-11-26 13:39:31 +0700

    fix bug

--- apis/diet.go
+++ apis/diet.go
@@ -1,6 +1,8 @@
 package apis
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo"
 	"github.com/masterraf21/dss-backend/middleware"
 	"github.com/masterraf21/dss-backend/models"
@@ -19,9 +21,16 @@ func NewDietRouter(dtr models.DietUsecase) *dietRouter {
 func (r *dietRouter) Mount(group *echo.Group) {
 	bearerVerify := middleware.CreateBearerVerify()
 
+	group.GET("/test", r.Test, bearerVerify)
 	group.POST("", r.FindDiet, bearerVerify)
 }
 
 func (r *dietRouter) FindDiet(c echo.Context) (err error) {
+	return
+}
+
+func (r *dietRouter) Test(c echo.Context) (err error) {
+	fmt.Println("TEST")
+
 	return
 }
--- middleware/jwtVerify.go
+++ middleware/jwtVerify.go
@@ -16,6 +16,76 @@ func CreateBearerVerify() echo.MiddlewareFunc {
 	})
 }
 
+// func BearerVerify(rsaPublicKey *rsa.PublicKey, mustAuthorized bool) echo.MiddlewareFunc {
+// 	return func(next echo.HandlerFunc) echo.HandlerFunc {
+// 		return func(c echo.Context) error {
+// 			if os.Getenv("NO_TOKEN") == "1" {
+// 				return next(c)
+// 			}
+
+// 			auth := c.Request().Header.Get("Authorization")
+
+// 			if len(auth) <= 0 {
+// 				return helper.NewResponse(http.StatusUnauthorized, "authorization is empty", nil).WriteResponse(c, 0, nil)
+// 			}
+
+// 			splitToken := strings.Split(auth, " ")
+// 			if len(splitToken) < 2 {
+// 				return helper.NewResponse(http.StatusUnauthorized, "authorization is empty", nil).WriteResponse(c, 0, nil)
+// 			}
+
+// 			if splitToken[0] != "Bearer" {
+// 				return helper.NewResponse(http.StatusUnauthorized, "authorization is invalid", nil).WriteResponse(c, 0, nil)
+// 			}
+
+// 			tokenStr := splitToken[1]
+// 			token, err := jwt.ParseWithClaims(tokenStr, &BearerClaims{}, func(token *jwt.Token) (interface{}, error) {
+// 				if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+// 					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+// 				}
+// 				return rsaPublicKey, nil
+// 			})
+
+// 			refreshToken := c.QueryParam("refresh_token")
+// 			if err != nil && refreshToken == "" && !strings.Contains(c.Path(), "token/refresh") {
+// 				if strings.Contains(err.Error(), "token") && strings.Contains(err.Error(), "expired") {
+// 					err = errors.New("Your session has expired. Please login again")
+// 				}
+// 				return helper.NewResponse(http.StatusUnauthorized, err.Error(), nil).WriteResponse(c, 0, nil)
+// 			}
+
+// 			if claims, ok := token.Claims.(*BearerClaims); err == nil && token.Valid && ok {
+// 				if mustAuthorized {
+// 					if claims.UserAuthorized {
+// 						c.Set("token", token)
+// 						return next(c)
+// 					}
+// 					fmt.Printf("%+v", claims)
+// 					return helper.NewResponse(http.StatusUnauthorized, "Resource need an authorised user", nil).WriteResponse(c, 0, nil)
+// 				}
+// 				c.Set("token", token)
+// 				return next(c)
+// 			} else if ve, ok := err.(*jwt.ValidationError); ok {
+// 				if refreshToken != "" && strings.Contains(c.Path(), "token/refresh") {
+// 					c.Set("token", token)
+// 					return next(c)
+// 				}
+// 				var errorStr string
+// 				if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+// 					errorStr = fmt.Sprintf("Invalid token format: %s", tokenStr)
+// 				} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
+// 					errorStr = "Token has been expired"
+// 				} else {
+// 					errorStr = fmt.Sprintf("Token Parsing Error: %s", err.Error())
+// 				}
+// 				return helper.NewResponse(http.StatusUnauthorized, errorStr, nil).WriteResponse(c, 0, nil)
+// 			} else {
+// 				return helper.NewResponse(http.StatusUnauthorized, "Unknown token error", nil).WriteResponse(c, 0, nil)
+// 			}
+// 		}
+// 	}
+// }
+
 // func TokenRefresherMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 // 	return func(c echo.Context) error {
 // 		// If the user is not authenticated (no user token data in the context), don't do anything.
